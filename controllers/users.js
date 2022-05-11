b2ec9b52e9b751a4c177b7b10d8f763310d045d8
Francisco <francisco.rivera@servitron.net>
2022-05-11 02:32:48 -0500

    Se implementa el API de Bienes Raices

--- controllers/users.js
+++ controllers/users.js
@@ -44,6 +44,56 @@ function getUserForLogin(req, res){
     });
 }
 
+function createUser(req, res){
+    var response = []; // Arreglo final de la respuesta de codigo 200.
+
+    //Obtenemos el cuerpo de la solicitud
+    var body = req.body;
+
+    console.log(body);
+
+    //Creamos la conexion de base de datos (mysql)
+    let connection = mysql.createConnection(config);
+
+    new Promise((resolve, reject) =>{
+
+        //Consulta sql (stored procedure)
+        let sql = `call usp_CreateUser(?,?,?,?,?,?,?,?)`;
+
+        console.log("Que onda");
+
+        connection.query(sql, [body.Username, body.UserLastName, body.UsersSex, body.UsersRoleId, body.UsersStatusId, body.UsersPassword, body.UsersEmail, body.UsersPhone], (error, results, fields) => {
+
+            //En caso de error en la ejecución//
+            if (error) {
+                console.error(error.message);
+
+                reject(res.status(500).send({
+                    error
+                }));
+            }
+
+            console.log(results);
+
+            //Convertimos en arreglo los resultados de salida del stored procedure.
+            const result = Object.values(JSON.parse(JSON.stringify(results[0])));
+
+            //Itero el resultado y los agrego a la salida final
+            result.forEach((v) => response.push(v));
+
+            //Resultado de codigo 200 (correcto)
+            resolve(res.status(201).send({
+                response
+            }));
+
+        });
+    });
+}
+
+function recoveryUser(req, res){
+    
+}
+
 module.exports = {
     getUserForLogin
 }
