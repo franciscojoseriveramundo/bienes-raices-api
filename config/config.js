5d59f2e2d2d97e1bb5d01bcad372576b080dd163
Francisco <francisco.rivera@servitron.net>
2022-05-11 02:46:30 -0500

    Se realiza la conexion remota a MYSQL.

--- config/config.js
+++ config/config.js
@@ -1,17 +1,19 @@
-/*
+
 let config = {
     host    : 'bkvr6afh90lntut7zlay-mysql.services.clever-cloud.com',
     user    : 'uccyyf00d6p1cs9k',
     password: 'bAiGtlCuannbB7tZDT6T',
     database: 'bkvr6afh90lntut7zlay'
   };
-*/
 
+
+/*
 let config = {
     host    : 'localhost',
     user    : 'francisco',
     password: 'peluchin1994',
     database: 'bienes_raices'
   };
+*/
 
 module.exports = config;
